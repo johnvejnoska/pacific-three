4c4cbe47fbfd7922e96ec8f8f30b049343510323
John Vejnoska <john.vejnoska@gmail.com>
2013-12-26 15:25:53 -0800

    Init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PacificThree::Application.config.secret_token = 'd841bc109f94c5ae425f2e0555afa158af3b40f6f5052ce07736a64ea0253cadbca3d9ac2751930b679cffe8dc34c25f34461059ca69c652498f1bc5ef2367cc'
